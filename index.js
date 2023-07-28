1bf1ef9ac735a10a5bb5b19eb41d7fffc3ab5fbd
Lana Kisel <svetlana.kisel.ca@gmail.com>
2023-07-28 15:05:52 -0700

    filter method lab

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Code your solution here
+const drivers = [
+    {name: "lana", homeTown: "Lu"},
+    {name: 'Mat', homeTown: "NY"},
+    {name: "Tim", homeTown: "Sydney"},
+    {name: "Poppy", homeTown: "Tokyo"}]
+
+function findMatching(collection, name) {
+    return collection.filter((aDriverName, index, originalArray) => {
+        if (aDriverName.toLowerCase() === name.toLowerCase()) { return true } else {
+            return false
+        };
+    })
+}
+
+console.log("final output:", findMatching(drivers, "poppy"))
+
+
+function fuzzyMatch(collection, letters) {
+    const lengthOfTheString = letters.length;
+    return collection.filter((driverName, index, originalArray) => {
+        if (driverName.substring(0, lengthOfTheString) === letters.substring(0, lengthOfTheString)) { return true } else {
+            return false
+        };
+    })
+}
+             
+function matchName(collection, name)
+ {
+    return collection.filter((driver) => {
+        if (driver.name === name) {
+            return true
+        } // else {
+        //     return false
+        // }
+    })
+
+ }    
